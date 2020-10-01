1ac34e981ca74515631da56cc1c640bfd7ad7e90
nekoreimu <33898438+nekoreimu@users.noreply.github.com>
2020-10-01 16:40:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,13 +14,14 @@
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git vmtools https://github.com/srchack/custom-packages' feeds.conf.default
-#git reset --hard cc2ddb220a
 git clone https://github.com/srchack/custom-packages package/custom-packages
 git clone https://github.com/jerrykuku/luci-app-vssr package/vssr
 git clone https://github.com/jerrykuku/lua-maxminddb package/lean/lua-maxminddb
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 git clone https://github.com/fw876/helloworld package/helloworld
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 #svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
 #svn co https://github.com/kenzok8/small/trunk/brook package/brook
 #svn co https://github.com/kenzok8/small/trunk/chinadns-ng package/chinadns-ng
